92fb9dfb2a6cf7734975223910616ee0ee78708b
ktlim <ktlim@git.lsstcorp.org>
2009-03-18 22:20:03 +0000

    Use proper ccdId and ampId.

--- python/lsst/ip/isr/IdentifyCalibrationProductsStage.py
+++ python/lsst/ip/isr/IdentifyCalibrationProductsStage.py
@@ -17,11 +17,13 @@ class IdentifyCalibrationProductsStage(Stage):
         eventName = self._policy.get("eventName")
         event = self.activeClipboard.get(eventName)
         when = DateTime(event.get("dateObs"))
+
+        ccdId = self.activeClipboard.get("ccdId")
+        ampId = self.activeClipboard.get("ampId")
         
         expTime = event.get("expTime")
         darkPolicy = self._policy.get("darkPolicy")
-        darkCalibList = self.cdb.lookup(when, "dark", event.get("ccdId"),
-                event.get("ampId"), all=True)
+        darkCalibList = self.cdb.lookup(when, "dark", ccdId, ampId, all=True)
         darkTimeList = []
         for d in darkCalibList:
             darkTimeList.append(d.expTime)
@@ -43,18 +45,15 @@ class IdentifyCalibrationProductsStage(Stage):
             raise RuntimeError, "Unrecognized darkPolicy: " + str(darkPolicy)
 
 
-        biasPath = self.cdb.lookup(when, "bias", event.get("ccdId"),
-                event.get("ampId"))
-        darkPath = self.cdb.lookup(when, "dark", event.get("ccdId"),
-                event.get("ampId"), expTime=darkExpTime)
-        defectPath = self.cdb.lookup(when, "defect", event.get("ccdId"),
-                event.get("ampId"))
-        flatPath = self.cdb.lookup(when, "flat", event.get("ccdId"),
-                event.get("ampId"), filter=event.get("filter"))
-#         fringePath = self.cdb.lookup(when, "fringe", event.get("ccdId"),
-#                 event.get("ampId"), filter=event.get("filter"))
-        linearizePath = self.cdb.lookup(when, "linearize", event.get("ccdId"),
-                event.get("ampId"))
+        biasPath = self.cdb.lookup(when, "bias", ccdId, ampId)
+        darkPath = self.cdb.lookup(when, "dark", ccdId, ampId,
+                expTime=darkExpTime)
+        defectPath = self.cdb.lookup(when, "defect", ccdId, ampId)
+        flatPath = self.cdb.lookup(when, "flat", ccdId, ampId,
+                filter=event.get("filter"))
+#         fringePath = self.cdb.lookup(when, "fringe", ccdId, ampId,
+#                 filter=event.get("filter"))
+        linearizePath = self.cdb.lookup(when, "linearize", ccdId, ampId)
 
         pathPrefix = LogicalLocation(self._policy.get("pathPrefix")).locString()
         calibData = PropertySet()
@@ -62,7 +61,7 @@ class IdentifyCalibrationProductsStage(Stage):
         calibData.set("darkPath", os.path.join(pathPrefix, darkPath))
         calibData.set("defectPath", os.path.join(pathPrefix, defectPath))
         calibData.set("flatPath", os.path.join(pathPrefix, flatPath))
-#         calibData.set("fringePath", fringePath)
+#         calibData.set("fringePath", os.path.join(pathPrefix, fringePath))
         calibData.set("linearizePath", os.path.join(pathPrefix, linearizePath))
 
         outputKey = self._policy.get("outputKey")
