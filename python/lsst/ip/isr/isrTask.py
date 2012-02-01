3581f012a43e2745d77fc3e732d1d5a44eb0a6d9
Paul Price <price@astro.princeton.edu>
2012-02-01 16:31:28 -0500

    Fix compilation errors in IsrTaskConfig.

--- python/lsst/ip/isr/isrTask.py
+++ python/lsst/ip/isr/isrTask.py
@@ -87,7 +87,7 @@ class IsrTaskConfig(pexConfig.Config):
         default = True,
     )
     keysToRemoveFromAssembledCcd = pexConfig.ListField(
-        str,
+        dtype = str,
         doc = "fields to remove from the metadata of the assembled ccd.",
         default = [],
     )
@@ -97,7 +97,7 @@ class IsrTaskConfig(pexConfig.Config):
         default = True,
     )
     methodList = pexConfig.ListField(
-        str,   
+        dtype = str,   
         doc = "The list of ISR corrections to apply in the order they should be applied",
         default = ["doConversionForIsr", "doSaturationDetection", "doOverscanCorrection", "doBiasSubtraction", "doVariance", "doDarkCorrection", "doFlatCorrection"],
     )
