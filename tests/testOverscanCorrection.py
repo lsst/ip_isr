779e13af6ea21646aa77874c5cfec0a823f42280
John K. Parejko <parejkoj@uw.edu>
2016-08-15 14:39:03 -0700

    py.test cleanups for testOverscanCorrection

--- tests/testOverscanCorrection.py
+++ tests/testOverscanCorrection.py
@@ -23,13 +23,13 @@
 #
 import unittest
 
-import lsst.utils.tests as tests
+import lsst.utils.tests
 import lsst.afw.image as afwImage
 import lsst.afw.geom as afwGeom
 import lsst.ip.isr as ipIsr
 
 
-class IsrTestCases(unittest.TestCase):
+class IsrTestCases(lsst.utils.tests.TestCase):
 
     def setUp(self):
         self.overscanKeyword = "BIASSEC"
@@ -164,19 +164,14 @@ class IsrTestCases(unittest.TestCase):
             self.checkPolyOverscanCorrectionY(fitType=fitType, order=5)
 
 
-def suite():
-    """Returns a suite containing all the test cases in this module."""
-    tests.init()
-
-    suites = []
-    suites += unittest.makeSuite(IsrTestCases)
-    suites += unittest.makeSuite(tests.MemoryTestCase)
-    return unittest.TestSuite(suites)
+class MemoryTester(lsst.utils.tests.MemoryTestCase):
+    pass
 
 
-def run(exit=False):
-    """Run the tests"""
-    tests.run(suite(), exit)
+def setup_module(module):
+    lsst.utils.tests.init()
+
 
 if __name__ == "__main__":
-    run(True)
+    lsst.utils.tests.init()
+    unittest.main()
