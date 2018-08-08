b1e524892c10682224da1723a2f99a512bd3d1f2
Christopher Waters <czw@princeton.edu>
2018-08-08 16:06:32 -0500

    Make IsrTask a valid CmdLineTask.
    The IsrTask was not usable as a CmdLineTask, as it was written to be
    called as a subtask of ProcessCcdTask.  This allows the default
    IsrTask to be retargeted to camera-specific versions of the ISR
    processing.  A new IsrWrapperTask now exists to hold IsrTask for
    retargetting in standalone processing, and both processCcd.py and
    runIsr.py now read from the same configuration files for each obs_
    package.  This has involved migrating configuration values from
    config/processCcd.py to config/isr.py for many obs_ packages.

--- bin.src/runIsr.py
+++ bin.src/runIsr.py
@@ -21,6 +21,7 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <https://www.gnu.org/licenses/>.
 #
+
 from lsst.ip.isr.isrTask import RunIsrTask
 
 RunIsrTask.parseAndRun()
--- python/lsst/ip/isr/isrTask.py
+++ python/lsst/ip/isr/isrTask.py
@@ -2213,6 +2213,7 @@ class FakeAmp(object):
         return float("NaN")
 
 
+
 class RunIsrConfig(pexConfig.Config):
     isr = pexConfig.ConfigurableField(target=IsrTask, doc="Instrument signature removal")
 
