fbf0bcd34b6d5252c0290ecc918e3e34fee83c1d
Leanne Guy <leanne.guy@lsst.org>
2019-09-13 13:48:04 -0700

    fixed method error and flake 8 issues

--- tests/test_flatAndIlluminationCorrection.py
+++ tests/test_flatAndIlluminationCorrection.py
@@ -19,22 +19,23 @@
 # the GNU General Public License along with this program.  If not,
 # see <http://www.lsstcorp.org/LegalNotices/>.
 #
-from __future__ import absolute_import, division, print_function
 
-from builtins import range
 import unittest
 
 import lsst.utils.tests
+import lsst.geom
 import lsst.afw.image as afwImage
-import lsst.afw.geom as afwGeom
 import lsst.ip.isr as ipIsr
+
+from lsst.afw.cameraGeom.testUtils import DetectorWrapper
+from lsst.ip.isr import IsrTask
 
 
 class IsrTestCases(unittest.TestCase):
 
     def setUp(self):
-        self.pmin = afwGeom.Point2I(1, 1)
-        self.pmax = afwGeom.Point2I(10, 10)
+        self.pmin = lsst.geom.Point2I(1, 1)
+        self.pmax = lsst.geom.Point2I(10, 10)
         self.flatScaleKeyword = "IMMODE"
         self.filenameKeyword = "filename"
 
@@ -45,10 +46,10 @@ class IsrTestCases(unittest.TestCase):
         del self.filenameKeyword
 
     def doFlat(self, scaling):
-        maskedImage = afwImage.MaskedImageF(afwGeom.Box2I(self.pmin, self.pmax))
+        maskedImage = afwImage.MaskedImageF(lsst.geom.Box2I(self.pmin, self.pmax))
         maskedImage.getImage().set(10)
 
-        flat = afwImage.MaskedImageF(afwGeom.Box2I(self.pmin, self.pmax))
+        flat = afwImage.MaskedImageF(lsst.geom.Box2I(self.pmin, self.pmax))
         flat.getImage().set(1)
         flatexposure = afwImage.ExposureF(flat, None)
         dmetadata = flatexposure.getMetadata()
@@ -60,7 +61,7 @@ class IsrTestCases(unittest.TestCase):
         width = maskedImage.getWidth()
         for j in range(height):
             for i in range(width):
-                self.assertAlmostEqual(maskedImage.getImage().get(i, j), 10 / (1./scaling), 5)
+                self.assertAlmostEqual(maskedImage.image[i, j, afwImage.LOCAL], 10 / (1./scaling), 5)
 
     def testFlat1(self):
         self.doFlat(scaling=10)
@@ -72,10 +73,10 @@ class IsrTestCases(unittest.TestCase):
         self.doFlat(scaling=3.7)
 
     def doIllum(self, scaling):
-        maskedImage = afwImage.MaskedImageF(afwGeom.Box2I(self.pmin, self.pmax))
+        maskedImage = afwImage.MaskedImageF(lsst.geom.Box2I(self.pmin, self.pmax))
         maskedImage.getImage().set(10)
 
-        illum = afwImage.MaskedImageF(afwGeom.Box2I(self.pmin, self.pmax))
+        illum = afwImage.MaskedImageF(lsst.geom.Box2I(self.pmin, self.pmax))
         illum.getImage().set(1)
         illumexposure = afwImage.ExposureF(illum, None)
         dmetadata = illumexposure.getMetadata()
@@ -87,7 +88,7 @@ class IsrTestCases(unittest.TestCase):
         width = maskedImage.getWidth()
         for j in range(height):
             for i in range(width):
-                self.assertAlmostEqual(maskedImage.getImage().get(i, j), 10 / (1./scaling), 5)
+                self.assertAlmostEqual(maskedImage.image[i, j, afwImage.LOCAL], 10 / (1./scaling), 5)
 
     def testIllum1(self):
         self.doIllum(scaling=10)
@@ -99,9 +100,6 @@ class IsrTestCases(unittest.TestCase):
         self.doIllum(scaling=3.7)
 
     def testGainAndReadnoise(self):
-        import lsst.afw.image as afwImage
-        from lsst.afw.cameraGeom.testUtils import DetectorWrapper
-        from lsst.ip.isr import IsrTask
 
         isrTask = IsrTask()
 
@@ -121,7 +119,7 @@ class IsrTestCases(unittest.TestCase):
             if gain <= 0:               # behave the same way as amp.setGain
                 gain = 1
 
-            self.assertEqual(raw.variance.get(0, 0), level/gain + readNoise**2)
+            self.assertEqual(raw.variance[0, 0, afwImage.LOCAL], level/gain + readNoise**2)
 
 class MemoryTester(lsst.utils.tests.MemoryTestCase):
     pass
