1e769cdf0a350dea01905fc84576bb7ccb48bd50
Eli Rykoff <erykoff@stanford.edu>
2024-08-29 20:04:45 -0700

    Add test for bright saturated and flooded flat.

--- tests/test_isrTaskLSST.py
+++ tests/test_isrTaskLSST.py
@@ -841,6 +841,80 @@ class IsrTaskLSSTTestCase(lsst.utils.tests.TestCase):
         # trail.
         self.assertLess(np.std(delta), 7.0)
 
+    def test_isrFloodedSaturated(self):
+        """Test ISR when the amps are completely flooded and
+        the parallel overscan region is also flooded.
+        """
+        # We are simulating a flat field.
+        # Note that these aren't very important because we are replacing
+        # the flux, but we may as well.
+        mock_config = self.get_mock_config_no_signal()
+        mock_config.doAddDark = True
+        mock_config.doAddFlat = True
+        # The doAddSky option adds the equivalent of flat-field flux.
+        mock_config.doAddSky = True
+
+        mock = isrMockLSST.IsrMockLSST(config=mock_config)
+        input_exp = mock.run()
+
+        isr_config = self.get_isr_config_minimal_corrections()
+        isr_config.doBootstrap = True
+        isr_config.doApplyGains = False
+        isr_config.doBias = True
+        isr_config.doDark = True
+        isr_config.doFlat = False
+        # Tun off saturation masking to simulate a PTC flat.
+        isr_config.doSaturation = False
+
+        amp_config = isr_config.overscanCamera.defaultDetectorConfig.defaultAmpConfig
+        parallel_overscan_saturation = amp_config.parallelOverscanConfig.parallelOverscanSaturationLevel
+
+        detector = input_exp.getDetector()
+        for i, amp in enumerate(detector):
+            # For half of the amps we are testing what happens when the
+            # parallel overscan region is above the configured saturation
+            # level; for the other half we are testing the other branch
+            # when it saturates below this level (which is a priori
+            # unknown).
+            if i < len(detector) // 2:
+                data_level = (parallel_overscan_saturation * 1.05
+                              + mock_config.biasLevel
+                              + mock_config.clockInjectedOffsetLevel)
+                parallel_overscan_level = (parallel_overscan_saturation * 1.01
+                                           + mock_config.biasLevel
+                                           + mock_config.clockInjectedOffsetLevel)
+            else:
+                data_level = 0.9 * parallel_overscan_saturation
+                parallel_overscan_level = 0.75 * data_level
+
+            input_exp[amp.getRawDataBBox()].image.array[:, :] = data_level
+            input_exp[amp.getRawParallelOverscanBBox()].image.array[:, :] = parallel_overscan_level
+
+        isr_task = IsrTaskLSST(config=isr_config)
+        with self.assertLogs(level=logging.WARNING) as cm:
+            result = isr_task.run(
+                input_exp.clone(),
+                bias=self.bias_adu,
+                dark=self.dark_adu,
+            )
+        self.assertEqual(len(cm.records), len(detector))
+
+        n_all = 0
+        n_level = 0
+        for record in cm.records:
+            if "All overscan pixels masked" in record.message:
+                n_all += 1
+            if "The level in the overscan region" in record.message:
+                n_level += 1
+
+        self.assertEqual(n_all, len(detector) // 2)
+        self.assertEqual(n_level, len(detector) // 2)
+
+        # And confirm that the post-ISR levels are high for each amp.
+        for amp in detector:
+            med = np.median(result.exposure[amp.getBBox()].image.array)
+            self.assertGreater(med, 50000.0)
+
     def get_mock_config_no_signal(self):
         """Get an IsrMockLSSTConfig with all signal set to False.
 
