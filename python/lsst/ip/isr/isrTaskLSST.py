9280be17b9c38fb20471df2a738360f583ea712a
Eli Rykoff <erykoff@stanford.edu>
2025-04-10 21:04:03 -0700

    Do simpler sat/suspect masking to avoid afw footprints.

--- python/lsst/ip/isr/isrTaskLSST.py
+++ python/lsst/ip/isr/isrTaskLSST.py
@@ -812,12 +812,8 @@ class IsrTaskLSST(pipeBase.PipelineTask):
             for maskName, maskThreshold in limits.items():
                 if not math.isnan(maskThreshold):
                     dataView = maskedImage.Factory(maskedImage, amp.getRawBBox())
-                    isrFunctions.makeThresholdMask(
-                        maskedImage=dataView,
-                        threshold=maskThreshold,
-                        growFootprints=0,
-                        maskName=maskName
-                    )
+                    toMask = (dataView.image.array >= maskThreshold)
+                    dataView.mask.array[toMask] |= dataView.mask.getPlaneBitMask(maskName)
 
             # Determine if we've fully masked this amplifier with SUSPECT and
             # SAT pixels.
