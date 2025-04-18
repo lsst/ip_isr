3a58d39981c0b7f4744d498ebe2e02e4d23b169c
Eli Rykoff <erykoff@stanford.edu>
2025-04-18 15:12:42 -0700

    Add transference of validPolygon from flats to images.
    This also will zero-out any NO_DATA pixels from the 100% vignetted region.

--- python/lsst/ip/isr/isrTaskLSST.py
+++ python/lsst/ip/isr/isrTaskLSST.py
@@ -2051,6 +2051,17 @@ class IsrTaskLSST(pipeBase.PipelineTask):
                 scalingType=self.config.flatScalingType,
                 userScale=self.config.flatUserScale,
             )
+
+            # Copy over valid polygon from flat if it is
+            # available, and set NO_DATA to 0.0 (which may
+            # be inherited from the flat in the fully
+            # vignetted region).
+            if (validPolygon := flat.info.getValidPolygon()) is not None:
+                ccdExposure.info.setValidPolygon(validPolygon)
+
+                noData = (ccdExposure.mask.array & ccdExposure.mask.getPlaneBitMask("NO_DATA")) > 0
+                ccdExposure.image.array[noData] = 0.0
+
             ccdExposure.metadata["LSST ISR FLAT APPLIED"] = True
             ccdExposure.metadata["LSST ISR FLAT SOURCE"] = flat.metadata.get("FLATSRC", "UNKNOWN")
 
