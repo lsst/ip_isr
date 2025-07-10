3eb5b8a2e3ee1214fac22bb3cecb2e0ba4af14e0
Matthias Wittgen <matthias.michael.wittgen@cern.ch>
2025-07-10 15:48:08 -0700

    Use classh

--- python/lsst/ip/isr/isr.cc
+++ python/lsst/ip/isr/isr.cc
@@ -37,7 +37,7 @@ namespace {
 
 template <typename PixelT>
 static void declareCountMaskedPixels(py::module& mod, std::string const& suffix) {
-    py::class_<CountMaskedPixels<PixelT>> cls(mod, ("CountMaskedPixels" + suffix).c_str());
+    py::classh<CountMaskedPixels<PixelT>> cls(mod, ("CountMaskedPixels" + suffix).c_str());
 
     cls.def("reset", &CountMaskedPixels<PixelT>::reset);
     cls.def("apply", &CountMaskedPixels<PixelT>::apply, "image"_a, "bitmask"_a);
