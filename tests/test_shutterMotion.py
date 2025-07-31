451ea72ae111ecaee900e39697adbe8695414a0c
Christopher Waters <czw@princeton.edu>
2025-07-31 09:52:53 -0700

    Add v2 support.

--- tests/test_shutterMotion.py
+++ tests/test_shutterMotion.py
@@ -30,7 +30,7 @@ from lsst.ip.isr import ShutterMotionProfile
 TESTDIR = os.path.abspath(os.path.dirname(__file__))
 
 
-class ShutterMotionTestCase(lsst.utils.tests.TestCase):
+class ShutterMotionV1TestCase(lsst.utils.tests.TestCase):
     def setUp(self):
         filename = os.path.join(TESTDIR, "data", "MC_O_20250526_000095_shutterMotionProfileOpen.json")
         self.calib = ShutterMotionProfile.readText(filename)
@@ -56,6 +56,36 @@ class ShutterMotionTestCase(lsst.utils.tests.TestCase):
         mid_accel, mid_position = self.calib.calculateMidpoint(modelName="motorEncoderFit")
         self.assertFloatsAlmostEqual(mid_accel, 0.45040944835186697)
         self.assertFloatsAlmostEqual(mid_position, 0.4497698788310154)
+
+
+class ShutterMotionV2TestCase(lsst.utils.tests.TestCase):
+    def setUp(self):
+        # We do not yet have true v2 shutter motion profiles, so this
+        # is a v1 file modified to match the expected changes.
+        filename = os.path.join(TESTDIR, "data", "MC_O_20250720_000554_shutterMotionProfileClose.json")
+        self.calib = ShutterMotionProfile.readText(filename)
+
+    def testRoundTrip(self):
+        outPath = tempfile.mktemp() + '.yaml'
+        self.calib.writeText(outPath)
+        newCalib = ShutterMotionProfile.readText(outPath)
+        self.assertEqual(self.calib, newCalib)
+
+        outPath = tempfile.mktemp() + '.fits'
+        self.calib.writeFits(outPath)
+        newCalib = ShutterMotionProfile.readFits(outPath)
+        self.assertEqual(self.calib, newCalib)
+
+    def testMidpointCalculation(self):
+        """
+        """
+        mid_accel, mid_position = self.calib.calculateMidpoint(modelName="hallSensorFit")
+        self.assertFloatsAlmostEqual(mid_accel, 0.4503675064864023)
+        self.assertFloatsAlmostEqual(mid_position, 0.45007949121581464)
+
+        mid_accel, mid_position = self.calib.calculateMidpoint(modelName="motorEncoderFit")
+        self.assertFloatsAlmostEqual(mid_accel, 0.4503689163377142)
+        self.assertFloatsAlmostEqual(mid_position, 0.45008433680071347)
 
 
 class MemoryTester(lsst.utils.tests.MemoryTestCase):
